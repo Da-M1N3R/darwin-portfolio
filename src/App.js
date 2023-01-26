4734f0794a9f68b3df17ec217bb04809c8937384
Frog <uranium322win@gmail.com>
2023-01-27 02:37:05 +0800

    Added Contact page

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
 import Home from "./pages/Home";
 import Projects from "./pages/Projects";
+import Contact from "./pages/Contact";
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
 function App() {
@@ -13,8 +14,8 @@ function App() {
         <Routes>
           <Route path='/' exact element={<Home/>} />
           <Route path='/projects' exact element={<Projects/>} />
-          {/* <Route path='/about' exact element={<About/>} />
-          <Route path='/contact' exact element={<Contact/>} /> */}
+          {/* <Route path='/about' exact element={<About/>} /> */}
+          <Route path='/contact' exact element={<Contact/>} />
         </Routes>
         <Footer />
       </Router>
---
+++ src/pages/Contact.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import WhatsappQR from '../assets/whatsappqr.png';
+import BannerImage from "../assets/RedandBlackBanner.png";
+import '../styles/Contact.css';
+
+function Contact() {
+  return (
+    <div className='contact' style={{ backgroundImage: `url(${BannerImage})` }}>
+        <div className='leftSide' style={{ backgroundImage: `url(${WhatsappQR})` }}></div>
+        <div className='rightSide'>
+            <h1> Email Me </h1>
+
+            <form id='contact-form' method='POST'>
+                <label htmlFor='name'> Full Name </label>
+                <input name='name' placeholder='Enter full name...' type="text" />
+                <label htmlFor='email'> Email </label>
+                <input name='email' placeholder='Enter email...' type="email" />
+                <label htmlFor='message'> Message </label>
+                <textarea rows="6" placeholder="Enter Message..." name='message' required></textarea>
+                <button type='submit'> Send Email </button>
+            </form>
+        </div>
+      
+    </div>
+  )
+}
+
+export default Contact
