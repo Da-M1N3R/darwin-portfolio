ad408496f02b059eacd8d152982d4dba8bb604ce
Frog <uranium322win@gmail.com>
2023-01-31 01:25:30 +0800

    project url button started -  incomplete

--- src/components/ProjectItem.js
+++ src/components/ProjectItem.js
@@ -1,11 +1,16 @@
 import React from 'react';
 
-function ProjectItem({ image, name, description }) {
+function ProjectItem({ image, name, web_url, description }) {
   return (
     <div className='projectItems'>
         <div className='projectImg' style={{ backgroundImage: `url(${image})` }}></div>
         <h1> {name} </h1>
         <p> Link: {description} </p>
+        <button className='projectUrl'>
+          <a href={ web_url }>
+            Link
+          </a>
+        </button>
     </div>
   )
 }
--- src/helpers/ProjectList.js
+++ src/helpers/ProjectList.js
@@ -9,21 +9,25 @@ export const ProjectList = [
     {
         name: "OPDT",
         image: P1,
+        web_url: 'https://fantasy.premierleague.com/',
         description: "Build your One Piece Dream Team.",
     },
     {
         name: "To-Dor",
         image: P2,
+        web_url: 'https://wc-react-todo-app.netlify.app/',
         description: "List your task today on here. List your task today on here. List your task today on here. List your task today on here. List your task today on here. List your task today on here. List your task today on here. ",
     },
     {
         name: "Content Aggregator",
         image: P3,
+        web_url: 'https://theweblist.net/',
         description: "Everything you want to know in one page.",
     },
     {
         name: "E-Commerce",
         image: P4,
+        web_url: 'https://shopee.com.my/',
         description: "Shopee clone.",
     },
 ]
